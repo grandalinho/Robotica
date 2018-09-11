7256d0812cc51ccbe873443f851edab49fcd16af
Jonas <grandalinho@gmail.com>
2018-09-11 17:10:49 -0300

    Agora o codigo do robo_desenho esta com os pinos PWM

--- Robo_Desenho/Robo_Desenho.ino
+++ Robo_Desenho/Robo_Desenho.ino
@@ -8,11 +8,14 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
+ pinMode(5,OUTPUT);
+ pinMode(6,OUTPUT);
+ pinMode(9,OUTPUT);
+ pinMode(10,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
